bc3ab50895cc15b5daa6cd7e7e5f6c813b4c03ed
AmirAli <amiralitehrani77@yahoo.com>
2026-03-07 13:56:05 -0500

    Created Navbar and main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import 'primeicons/primeicons.css';
+// import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +27,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+          <PrimeReactProvider>
+          {children}
+          </PrimeReactProvider>
+        </body>
     </html>
   );
 }
